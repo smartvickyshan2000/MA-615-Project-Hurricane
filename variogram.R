9bc4a4a1c1c4f67bb929c521e15da3144453c3e5
radonahue <radona@bu.edu>
2021-11-17 16:58:13 -0500

    Some updates still needed but rough fitting with the buoy data

--- variogram.R
+++ variogram.R
@@ -27,9 +27,18 @@ vm <- variogram(object=wind~1, Dolly_v, cutoff=10, width=0.3, map=T)
 
 plot(vm)
 
+
 #this is all of our point pairs
 
 v <- variogram(object=wind~1, Dolly_v)
 emp <- fit.variogram(v, model=vgm(psill=800, nugget=10, range=9, model="Gau"))  
 
 plot(v, emp, main="Semivariogram of Wind Speed", pch=20)
+
+View(BUOYS_6HOUR_SNAPSHOT)
+BUOYS_6HOUR_SNAPSHOT$lat<-as.numeric(BUOYS_6HOUR_SNAPSHOT$lat)
+BUOYS_6HOUR_SNAPSHOT$long<-as.numeric(BUOYS_6HOUR_SNAPSHOT$long)
+coordinates(BUOYS_6HOUR_SNAPSHOT) <-~lat+long  
+v2<-variogram(object=WSPD_adjusted~1, data=BUOYS_6HOUR_SNAPSHOT)
+
+str(BUOYS_6HOUR_SNAPSHOT$lat)

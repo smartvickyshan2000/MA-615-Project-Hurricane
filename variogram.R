44858d5983d604a3723d17ae34b9eef0208cc9c2
radonahue <radona@bu.edu>
2021-11-18 15:44:33 -0500

    2 semivariograms and interpretations

--- variogram.R
+++ variogram.R
@@ -2,43 +2,30 @@ library(tidyverse)
 library(maps)
 library(sp)
 library(sf)
-library(leaflet)
-library(htmlwidgets)
 library(drat)
 library(gstat)
-#addRepo("geanders")
-#install.packages("hurricaneexposuredata")
-#install.packages("hurricaneexposure")
-
 library(hurricaneexposuredata)
 library(hurricaneexposure)
 data("hurr_tracks")
-head(hurr_tracks)
 
 Dolly_v <- filter(as.data.frame(hurr_tracks), storm_id == "Dolly-2008")
 
 coordinates(Dolly_v) <- ~ latitude + longitude
 
-vc <- variogram(object=wind~1, Dolly_v, cloud=T)
-
-plot(vc)
-
-vm <- variogram(object=wind~1, Dolly_v, cutoff=10, width=0.3, map=T)
-
-plot(vm)
-
-
-#this is all of our point pairs
-
 v <- variogram(object=wind~1, Dolly_v)
 emp <- fit.variogram(v, model=vgm(psill=800, nugget=10, range=9, model="Gau"))  
+BUOYS_6HOUR_SNAPSHOT <- BUOYS_6HOUR_SNAPSHOT %>% mutate(long = as.numeric(long), lat = as.numeric(lat)) %>% mutate(long2 = -1*long)
 
-plot(v, emp, main="Semivariogram of Wind Speed", pch=20)
-
-View(BUOYS_6HOUR_SNAPSHOT)
-BUOYS_6HOUR_SNAPSHOT$lat<-as.numeric(BUOYS_6HOUR_SNAPSHOT$lat)
-BUOYS_6HOUR_SNAPSHOT$long<-as.numeric(BUOYS_6HOUR_SNAPSHOT$long)
 coordinates(BUOYS_6HOUR_SNAPSHOT) <-~lat+long  
+
 v2<-variogram(object=WSPD_adjusted~1, data=BUOYS_6HOUR_SNAPSHOT)
 
-str(BUOYS_6HOUR_SNAPSHOT$lat)
+emp2<-fit.variogram(v, model=vgm(psill=80, nugget=55, range=2.0, model="Gau")) 
+
+plot(v, emp, main="Semivariogram of 1 min Max Wind Speed (Hurr_Tracks Data)", pch=20)
+
+#This is a semivariogram that shows that generally as distance between points in the Hurr tracks dataset increases, the difference in their maximum sustained wind speed also increases, indicating a possible spatial relationship. Extrapolating from this plot further is difficult because the data violates the stationarity assumption required for kriging, which states that different regions contained in the data cannot have their outcome values changing at different rates than others, and a hurricane that moves around a region means that change is occurring at different rates depending on where it is at a given moment.
+
+plot(v2, emp2, main="Semivariogram of Average Wind Speed (Buoy Data)", pch=20)
+
+#This semivariogram does not have a traditional pattern like in the first example, the observed nugget value is high, indicating that there is a lot of noise in the data, and we can not say if there is a possible spatial relationship between buoy distance and average weed speed. 
